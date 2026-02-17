eea53181de4b2ec54a19b69f0f5311e35406d50f
jamescourtois <james.r.courtois@gmail.com>
2026-02-17 13:23:32 -0600

    Hello my besto frendo

---
+++ source/_layouts/main.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="{{ $page->description ?? $page->siteDescription }}">
+
+        <meta property="og:title" content="{{ $page->title ? $page->title . ' | ' : '' }}{{ $page->siteName }}"/>
+        <meta property="og:type" content="{{ $page->type ?? 'website' }}" />
+        <meta property="og:url" content="{{ $page->getUrl() }}"/>
+        <meta property="og:description" content="{{ $page->description ?? $page->siteDescription }}" />
+
+        <title>{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}</title>
+
+        <link rel="home" href="{{ $page->baseUrl }}">
+        <link rel="icon" href="/favicon.ico">
+        <link href="/blog/feed.atom" type="application/atom+xml" rel="alternate" title="{{ $page->siteName }} Atom Feed">
+
+        @if ($page->production)
+            <!-- Insert analytics code here -->
+        @endif
+
+        @viteRefresh()
+        <link rel="stylesheet" href="{{ vite('source/_assets/css/main.css') }}">
+        <script defer type="module" src="{{ vite('source/_assets/js/main.js') }}"></script>
+    </head>
+
+    <body class="flex flex-col justify-between min-h-screen bg-gray-100 text-gray-800 leading-normal font-sans">
+        <header class="flex items-center shadow bg-white border-b h-24 py-4" role="banner">
+            <div class="container flex items-center max-w-8xl mx-auto px-4 lg:px-8">
+                <div class="flex items-center">
+                    <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
+                        <img class="h-8 md:h-10 mr-3" src="/assets/img/frendo_logo.svg" alt="{{ $page->siteName }} logo" />
+
+                        <span class="sr-only">{{ $page->siteName }}</span>
+                    </a>
+                </div>
+
+                <div id="vue-search" class="flex flex-1 justify-end items-center">
+                    <!-- @include('_components.search') -->
+
+                    @include('_nav.menu')
+
+                    @include('_nav.menu-toggle')
+                </div>
+            </div>
+        </header>
+
+        @include('_nav.menu-responsive')
+
+        <main role="main" class="flex-auto w-full container max-w-4xl mx-auto py-16 px-6">
+            @yield('body')
+        </main>
+
+        <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
+            <ul class="flex flex-col md:flex-row justify-center list-none">
+                <li class="md:mr-2">
+                    &copy; Frendo Web Development {{ date('Y') }}.
+                </li>
+            </ul>
+        </footer>
+        @stack('scripts')
+    </body>
+</html>
