aac3e277cd5b381cc72a02b2cb0118b1351a0549
jamescourtois <james.r.courtois@gmail.com>
2026-03-15 13:02:32 -0500

    Perfect contact card page

--- source/contact-card.blade.php
+++ source/contact-card.blade.php
@@ -5,20 +5,19 @@ description: Let's keep in touch
 @extends('_layouts.main')
 
 @section('body')
-	<div class="flex flex-col items-center justify-center text-center">
+	<div x-data="{clicked:false}" class="flex flex-col items-center justify-center text-center">
 			<h1 class="uppercase text-xs m-0 font-normal tracking-widest">Contact Card</h1>
 			<h2 class="mt-3 leading-[2.8rem] text-5xl mb-5">Let's keep in touch.</h2>
 			
 			<a href="{{ $page->baseUrl . '/frendo.vcf' }}"
 				download="frendo.vcf"
 				type="text/vcard"
-				target="_blank"
-				x-data="{clicked:false}"
+				target="_self"
 				@click="clicked = true"
-				x-text="clicked ? 'Saved!' : 'Add Frendo as a Contact '"
+				x-text="clicked ? 'Frendo.vcf Saved!' : 'Add Frendo as a Contact'"
 				class="min-w-[260px] mt-4 mb-2 text-xl px-5 py-3 btn-special text-white rounded-full font-bold">
 		</a>
-		<span class="text-xs text-gray-500">(Save the Frendo.vcf contact card to your device.)</span>
+		<span x-text="clicked ? 'Check your downloads folder and open the contact card.' : 'Save the Frendo.vcf contact card to your device.'" class="text-xs text-gray-500"></span>
 			
 		<ul :class="$store.theme.dark ? 'bg-gray-800' : 'bg-blue-100'" class="list-none text-3xl font-bold mt-8 p-8">
 			<li><a class="active-text-green-500" href="tel:+13142494760">(314) 249-4760</a></li>
